0ed521e0652e881a8db304f6328096db05f08e4c
Reboot-Codes <git@reboot-codes.com>
2025-03-14 00:11:08 -0700

    Formatting and properzed support :P

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,21 @@
 #![feature(stmt_expr_attributes)]
 #![feature(let_chains)]
 
-pub mod server;
 pub mod arbiter;
+pub mod server;
 mod utils;
 
 pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+  left + right
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+  #[test]
+  fn it_works() {
+    let result = add(2, 2);
+    assert_eq!(result, 4);
+  }
 }
